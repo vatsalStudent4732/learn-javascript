040dc8caf7ad55532e95c3d2228b06780d1dbf01
vatsalStudent4732 <vatsal.brave47322gmail.com>
2024-08-13 16:57:02 +0000

    type conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = "Hello";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueScore = Number(score);
+
+// console.log(typeof valueScore);
+// console.log(valueScore);
+
+// 33 => 33
+// 33abc => NaN
+// true => 1 / false => 0
+
+// let isLoggedIn = "";
+
+// let booleanisloggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanisloggedIn);
+// console.log(booleanisloggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+//"hello" => true
+
+let someNum = 33;
+let strSomeNum = String(someNum);
+console.log(typeof strSomeNum);
+console.log(strSomeNum);
