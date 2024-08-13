b6bf4ed3eaf8eb935a4f7b15102cb5e6b4328c13
vatsalStudent4732 <vatsal.brave47322gmail.com>
2024-08-13 17:17:30 +0000

    operation in js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,47 @@
 // "" => false
 //"hello" => true
 
-let someNum = 33;
-let strSomeNum = String(someNum);
-console.log(typeof strSomeNum);
-console.log(strSomeNum);
+// let someNum = 33;
+// let strSomeNum = String(someNum);
+// console.log(typeof strSomeNum);
+// console.log(strSomeNum);
+
+// operations ------------------------
+
+let value = 3;
+let negativeValue = -value;
+// console.log(negativeValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 / 2);
+// console.log(2 * 2);
+// console.log(2 % 2);
+// console.log(2**2);
+
+// let str1 = "hello";
+// let str2 = "Vatsal";
+
+// let str3 = str1 + ' ' + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 1 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3;
+
+num1 = num2 = num3 = 4;
+
+console.log(num1, num2, num3);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+//link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
